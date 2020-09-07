419ec81ae0e003b2a98a1a2aa9432fa3cdcb6953
Dave Gill <david.g.h.gill@gmail.com>
2020-09-07 16:15:13 +0100

    Migration and Model for Particpant

---
+++ app/Participant.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\UseUuid;
+
+class Participant extends Model
+{
+  use UseUuid;
+
+  protected $fillable = [
+      'group_id', 'user_id',
+  ];
+}

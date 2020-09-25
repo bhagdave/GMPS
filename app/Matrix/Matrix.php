25975f321167bce2ba9c6e21fc90f8e456e9f4ff
Dave Gill <david.g.h.gill@gmail.com>
2020-09-25 17:17:35 +0100

    Service Provider and config for Matrix

---
+++ app/Matrix/Matrix.php
@@ -0,0 +1,45 @@
+<?php 
+namespace App\Matrix;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Exception;
+
+class Matrix{
+
+    public $session;
+    protected $client;
+    private $baseUrl;
+    private $domain;
+
+    /**
+     * Constructs a new matrix api instance
+     *
+     * @matrix
+     * @param string $domain
+     * @throws Exceptions\InvalidConfigurationException
+     */
+    public function __construct($domain)
+    {
+        $this->validateConstructorArgs($domain);
+        $this->domain = $domain;
+        $this->setupResources();
+    }
+
+    private function validateConstructorArgs($domain)
+    {
+        if (!isset($domain)) {
+            throw new Exception("Domain is empty.");
+        }
+    }
+
+    private function setupResources(){
+        $this->baseUrl = $this->domain.'_matrix/client/r0';
+        $this->client = new Client(['verify' => false]);
+    }
+
+    public function showDebugInfo(){
+        echo("MatrixServiceProviderDebugInfo<br>");
+        echo("BaseUrl:$this->baseUrl");
+    }
+}

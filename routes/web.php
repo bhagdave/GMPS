6bbcb2cd9489a4bb408a116f98d9b975e3ddff3b
Dave Gill <david.g.h.gill@gmail.com>
2020-11-14 15:39:10 +0000

    Put routes into groups#

--- routes/web.php
+++ routes/web.php
@@ -24,20 +24,28 @@ Route::get('/profile', 'HomeController@profile')->name('profile')->middleware('a
 Route::post('/profile', 'HomeController@saveProfile')->middleware('auth');
 
 Route::get('/groups', 'GroupController@index')->name('groups')->middleware('auth');
-Route::get('/group/add', 'GroupController@add')->middleware('auth');
-Route::post('/group/add', 'GroupController@store')->middleware('auth');
-Route::get('/group/{uuid}', 'GroupController@view')->middleware('auth');
-Route::get('/group/invite/{uuid}', 'ParticipantController@invite')->middleware('auth');
+Route::prefix('group')->middleware('auth')->group(function(){
+    Route::get('/add', 'GroupController@add');
+    Route::post('/add', 'GroupController@store');
+    Route::get('/{uuid}', 'GroupController@view');
+    Route::get('/invite/{uuid}', 'ParticipantController@invite');
+});
 
-Route::post('/participant/invite/{uuid}', 'ParticipantController@sendInvite')->middleware('auth');
-Route::get('/participant/invite/{uuid}/accept/{email}', 'ParticipantController@accept')->name('participant.accept');
-Route::post('/participant/accept/invite', 'ParticipantController@registerFromAccept')->name('participant.accept.invite');
-Route::get('/participant/remove/{uuid}/{userid}', 'ParticipantController@remove')->middleware('auth');
+Route::prefix('participant')->group(function(){
+    Route::post('/invite/{uuid}', 'ParticipantController@sendInvite')->middleware('auth');
+    Route::get('/invite/{uuid}/accept/{email}', 'ParticipantController@accept')->name('participant.accept');
+    Route::post('/accept/invite', 'ParticipantController@registerFromAccept')->name('participant.accept.invite');
+    Route::get('/remove/{uuid}/{userid}', 'ParticipantController@remove')->middleware('auth');
+});
 
-Route::get('/organisation/{uuid}', 'OrganisationController@view')->middleware('auth');
-Route::get('/organisation/invite/{uuid}', 'OrganisationController@inviteUser')->middleware('auth');
+Route::prefix('organisation')->middleware('auth')->group(function(){
+    Route::get('/{uuid}', 'OrganisationController@view');
+    Route::get('/invite/{uuid}', 'OrganisationController@inviteUser');
+});
 
-Route::delete('/user/delete', 'UserController@delete')->middleware('auth');
-Route::post('/user/invite/{uuid}', 'UserController@sendInvite')->middleware('auth');
-Route::get('/user/invite/{uuid}/accept/{email}', 'UserController@accept')->name('user.accept');
-Route::post('/user/accept/invite', 'UserController@registerFromAccept')->name('user.accept.invite');
+Route::prefix('user')->group(function(){
+    Route::delete('/delete', 'UserController@delete')->middleware('auth');
+    Route::post('/invite/{uuid}', 'UserController@sendInvite')->middleware('auth');
+    Route::get('/invite/{uuid}/accept/{email}', 'UserController@accept')->name('user.accept');
+    Route::post('/accept/invite', 'UserController@registerFromAccept')->name('user.accept.invite');
+});
